34e95979b55a87fc3b68519405b2344092ce1af1
团子 <876924196@qq.com>
2026-08-15 11:08:51 +0800

    add benzhi.Dockerfile + benzhi.build_docker.sh

---
+++ benzhi.build_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+IMAGE_NAME="${1:-go-score-bug}"
+PLATFORM="${2:-linux/amd64}"
+echo "Building Docker image: $IMAGE_NAME for $PLATFORM"
+docker build --platform "$PLATFORM" -t "$IMAGE_NAME:latest" -f benzhi.Dockerfile .
+echo "Build complete: $IMAGE_NAME:latest ($PLATFORM)"
+echo "Run with: docker run -it $IMAGE_NAME:latest"
